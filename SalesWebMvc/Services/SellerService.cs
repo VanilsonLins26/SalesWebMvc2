48423ad71eeda8836da49634499d998e8ed892c6
vanilson <vanilson.liins@gmail.com>
2024-10-10 10:22:01 -0300

    Editar os vendedores

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -40,5 +40,22 @@ namespace SalesWebMvc.Services
             _context.Remove(obj);
             _context.SaveChanges();
         }
+
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new DirectoryNotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbUpdateConcurrencyException(e.Message);
+            }
+        }
     }
 }
