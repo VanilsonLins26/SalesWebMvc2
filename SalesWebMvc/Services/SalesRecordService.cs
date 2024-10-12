e2ca8bbec75d5f81689fb93525d52f1168cdd5a2
vanilson <vanilson.liins@gmail.com>
2024-10-12 00:16:41 -0300

    adicao da busca composta

--- SalesWebMvc/Services/SalesRecordService.cs
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -26,6 +26,20 @@ namespace SalesWebMvc.Services
             }
             return await result.Include(x => x.Seller).Include(x => x.Seller.Department).OrderByDescending(x => x.Date).ToListAsync();
         }
-    }
 
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? mindate, DateTime? MaxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (mindate.HasValue)
+            {
+                result = result.Where(x => x.Date >= mindate.Value);
+            }
+            if (MaxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= MaxDate.Value);
+            }
+            return await result.Include(x => x.Seller).Include(x => x.Seller.Department).OrderByDescending(x => x.Date).GroupBy(x => x.Seller.Department).ToListAsync();
+        }
+    }
+   
 }
